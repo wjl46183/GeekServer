f3fd66d6621dee4d1c02b86498c071550252dfa4
wangjinliang <wjl46183@gmail.com>
2024-12-11 10:21:37 +0800

    mod - 清理无用代码

--- Geek.Server.Core/Comps/CompRegister.cs
+++ Geek.Server.Core/Comps/CompRegister.cs
@@ -21,16 +21,6 @@ namespace Geek.Server.Core.Comps
         /// </summary>
         internal static readonly Dictionary<Type, ActorType> CompActorDic = new();
 
-        /// <summary>
-        /// func -> CompTypes
-        /// </summary>
-        private static readonly Dictionary<int, HashSet<Type>> FuncCompDic = new();
-
-        /// <summary>
-        /// CompType -> func
-        /// </summary>
-        private static readonly Dictionary<Type, short> CompFuncDic = new();
-
         public static ActorType GetActorType(Type compType)
         {
             CompActorDic.TryGetValue(compType, out var actorType);
@@ -58,18 +48,7 @@ namespace Geek.Server.Core.Comps
                     var actorType = compAttr.ActorType;
                     var compTypes = ActorCompDic.GetOrAdd(actorType);
                     compTypes.Add(type);
-
                     CompActorDic[type] = actorType;
-
-                    if (actorType == ActorType.Role)
-                    {
-                        if (type.GetCustomAttribute(typeof(FuncAttribute)) is FuncAttribute funcAttr)
-                        {
-                            var set = FuncCompDic.GetOrAdd(funcAttr.func);
-                            set.Add(type);
-                            CompFuncDic[type] = funcAttr.func;
-                        }
-                    }
                 }
                 else
                 {
@@ -94,12 +73,6 @@ namespace Geek.Server.Core.Comps
                         {
                             throw new Exception($"{compType}未实现agent");
                         }
-
-                        //if (actorType > ActorType.Separator)
-                        //{
-                        //    Log.Info($"激活全局组件：{actorType} {compType}");
-                        //    await ActorMgr.GetCompAgent(agentType, actorType);
-                        //}
                     }
                     if (actorType > ActorType.Separator)
                     {
@@ -114,30 +87,6 @@ namespace Geek.Server.Core.Comps
                 Log.Error($"激活全局组件并检测组件是否都包含Agent实现失败");
                 throw;
             }
-        }
-
-        public static Task ActiveRoleComps(ICompAgent compAgent, HashSet<short> openFuncSet)
-        {
-            return ActiveComps(compAgent.Owner.Actor,
-                t => !CompFuncDic.TryGetValue(t, out var func)
-                || openFuncSet.Contains(func));
-            //foreach (var compType in GetComps(ActorType.Role))
-            //{
-            //    bool active;
-            //    if (CompFuncDic.TryGetValue(compType, out var func))
-            //    {
-            //        active = openFuncSet.Contains(func);
-            //    }
-            //    else
-            //    {
-            //        active = true;
-            //    }
-            //    if (active)
-            //    {
-            //        var agentType = HotfixMgr.GetAgentType(compType);
-            //        await compAgent.GetCompAgent(agentType);
-            //    }
-            //}
         }
 
         internal static async Task ActiveComps(Actor actor, Func<Type, bool> predict = null)
